298a7db23030b32975261d7697ab97f25c951a88
NickGreen <nick.green@automattic.com>
2022-06-20 17:28:54 -0700

    Add Stop Emails functionality

--- safety-net.php
+++ safety-net.php
@@ -22,6 +22,7 @@ require_once __DIR__ . '/includes/admin.php';
 require_once __DIR__ . '/includes/common.php';
 require_once __DIR__ . '/includes/delete.php';
 require_once __DIR__ . '/includes/utilities.php';
+require_once __DIR__ . '/includes/stop-emails.php';
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	require_once __DIR__ . '/includes/cli/anonymize.php';
