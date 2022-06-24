c025a8a6648f14541d52c2acf825066788cfe4d3
NickGreen <nick.green@automattic.com>
2022-06-24 13:50:17 -0700

    phpcs fixes

--- includes/deactivate-plugins.php
+++ includes/deactivate-plugins.php
@@ -14,7 +14,7 @@ function deactivate_plugins() {
 	$all_installed_plugins = array_keys( get_plugins() );
 
 	// blacklist can be partial matches, i.e. 'paypal' will match with any plugin that has 'paypal' in the slug
-	$blacklisted_plugins = array( 'paypal', 'stripe', 'affirm', 'smtp', 'in-stock-mailer-for-wc', 'klaviyo', 'wp-mail-bank', 'mailchimp', 'mailgun', 'metorik', 'sendinblue', 'wp-sendgrid-mailer', 'socketlabs', 'shipstation', 'wp-console', 'wp-ses', 'algolia', 'zapier',  );
+	$blacklisted_plugins = array( 'paypal', 'stripe', 'affirm', 'smtp', 'in-stock-mailer-for-wc', 'klaviyo', 'wp-mail-bank', 'mailchimp', 'mailgun', 'metorik', 'sendinblue', 'wp-sendgrid-mailer', 'socketlabs', 'shipstation', 'wp-console', 'wp-ses', 'algolia', 'zapier' );
 	$blacklisted_plugins = apply_filters( 'safety_net_blacklisted_plugins', $blacklisted_plugins );
 
 	// let's tack on all the Woo payment methods, in case we can deactivate any of those too
@@ -22,7 +22,7 @@ function deactivate_plugins() {
 		$installed_payment_methods = array_keys( WC()->payment_gateways->payment_gateways() );
 		foreach ( $installed_payment_methods as $key => $installed_payment_method ) {
 			$installed_payment_method = str_replace( '_', '-', $installed_payment_method );
-			$blacklisted_plugins[] = $installed_payment_method;
+			$blacklisted_plugins[]    = $installed_payment_method;
 		}
 	}
 
@@ -38,17 +38,16 @@ function deactivate_plugins() {
 
 				// remove plugin silently from active plugins list without triggering hooks
 				$current = get_option( 'active_plugins', array() );
+				// phpcs:ignore WordPress.PHP.StrictInArray.MissingTrueStrict
 				$key = array_search( $installed_plugin, $current );
 				if ( false !== $key ) {
 					array_splice( $current, $key, 1 );
 				}
-				update_option('active_plugins', $current);
+				update_option( 'active_plugins', $current );
 				break; // break out of nested loop once plugin has been deactivated
 
 			}
-
 		}
-
 	}
 
 }
@@ -57,7 +56,7 @@ function deactivate_plugins() {
 * Clear options such as API keys so that plugins won't talk to 3rd parties
 */
 function scrub_options() {
-	$options_to_clear = array( 'woocommerce_shipstation_auth_key', 'woocommerce_braintree_paypal_settings', 'woocommerce_braintree_credit_card_settings', 'klaviyo_settings', 'klaviyo_api_key', 'woocommerce_stripe_account_settings', 'woocommerce_stripe_api_settings', 'woocommerce_stripe_settings', 'woocommerce_ppcp-gateway_settings', 'woocommerce-ppcp-settings', 'woocommerce_paypal_settings', 'woocommerce_woocommerce_payments_settings', );
+	$options_to_clear = array( 'woocommerce_shipstation_auth_key', 'woocommerce_braintree_paypal_settings', 'woocommerce_braintree_credit_card_settings', 'klaviyo_settings', 'klaviyo_api_key', 'woocommerce_stripe_account_settings', 'woocommerce_stripe_api_settings', 'woocommerce_stripe_settings', 'woocommerce_ppcp-gateway_settings', 'woocommerce-ppcp-settings', 'woocommerce_paypal_settings', 'woocommerce_woocommerce_payments_settings' );
 	$options_to_clear = apply_filters( 'safety_net_options_to_clear', $options_to_clear );
 
 	foreach ( $options_to_clear as $option ) {
