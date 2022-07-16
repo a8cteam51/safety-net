c458427ebe9d175b82dac474c644a6347811e76c
Nate Allen <nate.allen@automattic.com>
2022-07-16 18:34:30 -0400

    Fire a hook when plugin is ready

--- safety-net.php
+++ safety-net.php
@@ -20,6 +20,7 @@ define( 'SAFETY_NET_BASENAME', plugin_basename(__FILE__) );
 require_once __DIR__ . '/includes/library/wp-background-processing/wp-background-processing.php';
 require_once __DIR__ . '/includes/anonymize.php';
 require_once __DIR__ . '/includes/admin.php';
+require_once __DIR__ . '/includes/bootstrap.php';
 require_once __DIR__ . '/includes/common.php';
 require_once __DIR__ . '/includes/delete.php';
 require_once __DIR__ . '/includes/utilities.php';
@@ -34,4 +35,5 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	require_once __DIR__ . '/includes/cli/anonymize.php';
 }
 
-
+// Fire a hook now that the plugin is ready.
+do_action( 'safety_net_loaded' );
