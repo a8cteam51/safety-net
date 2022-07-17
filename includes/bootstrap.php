881955a284532949c0f8902e9d6c6bbf287e6279
Nate Allen <nate.allen@automattic.com>
2022-07-16 22:43:32 -0400

    Add 'maybe_scrub_options' and 'maybe_disable_plugins' functions

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -10,6 +10,8 @@ use SafetyNet\Background_Anonymize_Customer;
 use SafetyNet\Background_Anonymize_Order;
 use SafetyNet\Background_Anonymize_User;
 
+add_action( 'safety_net_loaded', __NAMESPACE__ . '\maybe_scrub_options' );
+add_action( 'safety_net_loaded', __NAMESPACE__ . '\maybe_disable_plugins' );
 add_action( 'safety_net_loaded', __NAMESPACE__ . '\maybe_anonymize_data' );
 add_action( 'plugins_loaded', __NAMESPACE__ . '\instantiate_background_classes' );
 
@@ -23,11 +25,57 @@ function instantiate_background_classes() {
 }
 
 /**
+ * Determines if options should be scrubbed.
+ *
+ * Options will be scrubbed if we're on staging, development, or local AND it hasn't already been anonymized.
+ */
+function maybe_scrub_options() {
+	// If options have already been scrubbed, skip.
+	if ( get_option( 'safety_net_options_scrubbed' ) ) {
+		return;
+	}
+
+	// If we're not on staging, development, or a local environment, give warning and return.
+	if ( in_array( wp_get_environment_type(), array( 'staging', 'development', 'local' ), true ) ) {
+		_doing_it_wrong(
+			__FUNCTION__,
+			esc_html__( 'Safety Net plugin should not be run on production! Remove plugin or set WP_ENVIRONMENT_TYPE correctly.', 'safety-net' ),
+			'1.0.0-beta.3'
+		);
+		return;
+	}
+
+	do_action( 'safety_net_scrub_options' );
+}
+
+/**
+ * Determines if deny-listed plugins should be disabled.
+ *
+ * Plugins will be disabled if we're on staging, development, or local AND it hasn't already been anonymized.
+ */
+function maybe_disable_plugins() {
+	// If plugins have already been disabled, skip.
+	if ( get_option( 'safety_net_plugins_disabled' ) ) {
+		return;
+	}
+
+	// If we're not on staging, development, or a local environment, give warning and return.
+	if ( in_array( wp_get_environment_type(), array( 'staging', 'development', 'local' ), true ) ) {
+		_doing_it_wrong(
+			__FUNCTION__,
+			esc_html__( 'Safety Net plugin should not be run on production! Remove plugin or set WP_ENVIRONMENT_TYPE correctly.', 'safety-net' ),
+			'1.0.0-beta.3'
+		);
+		return;
+	}
+
+	do_action( 'safety_net_disable_plugins' );
+}
+
+/**
  * Determines if data should be anonymized.
  *
  * Data will be anonymized if we're on staging, development, or local AND it hasn't already been anonymized.
- *
- * @return void
  */
 function maybe_anonymize_data() {
 	// If data has already been anonymized, skip.
@@ -45,6 +93,6 @@ function maybe_anonymize_data() {
 		return;
 	}
 
-	// Fire hook to let plugin know to anonymize data.
-	do_action( 'safety_net_anonymize' );
+	// Fire hooks to let plugin know to anonymize data.
+	do_action( 'safety_net_anonymize_data' );
 }
