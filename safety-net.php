a3f793649112c0f11da67bce9023204049ad2d39
Nick <nick.green@automattic.com>
2022-11-10 15:44:18 -0800

    Bump to 1.4.0

--- safety-net.php
+++ safety-net.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Safety Net
  * Description: For Team51 Development Sites. Deletes user data and more!
- * Version: 1.3.0
+ * Version: 1.4.0
  * Author: WordPress.com Special Projects
  * Author URI: https://wpspecialprojects.wordpress.com
  * Text Domain: safety-net
