fd45707368aa3c48db129a9c64135730e67a41e1
Donna Peplinskie <donna.peplinskie@automattic.com>
2025-05-28 12:07:44 -0400

    Disable Jetpack Sync

--- includes/common.php
+++ includes/common.php
@@ -9,9 +9,12 @@ add_filter( 'send_email_change_email', '__return_false' );
 add_filter(
 	'jetpack_subscriptions_exclude_all_categories_except',
 	function () {
-		return array( 'non-existing' );	
+		return array( 'non-existing' );
 	}
 );
+
+// Disable Jetpack Sync.
+update_option( 'jetpack_sync_settings_disable', 1 );
 
 // Discourage search engines from indexing the site and disallow the entire site in robots.txt.
 add_filter( 'option_blog_public', '__return_zero' );
